98641c836a52fa3ca0092f619887b6148b1b4b3d
itu-itis24-taylan23 <taylan23@itu.edu.tr>
2025-07-04 23:07:29 +0300

    1

---
+++ Makefile
@@ -0,0 +1,32 @@
+SRCS	=	main.c \
+			init.c \
+			start_routines.c \
+			philo_routines.c \
+			utils.c \
+			arg_utils.c \
+			end.c
+
+OBJS	= $(SRCS:.c=.o)
+
+CC		= cc
+
+CFLAGS	= -g -Wall -Wextra -Werror -pthread
+
+NAME	= philo
+
+all: $(NAME)
+
+$(NAME): $(OBJS)
+	@$(CC) $(CFLAGS) $(OBJS) -o $(NAME)
+
+clean:
+	@echo "[DELETING...]"
+	rm -rf $(OBJS)
+	@echo "[DELETED]"
+
+fclean: clean
+	rm -rf $(NAME)
+
+re: fclean all
+
+.PHONY: all fclean clean re
---
+++ philo_routines.c
@@ -0,0 +1,55 @@
+#include "philo.h"
+
+void philo_think(t_philo *philo)
+{
+    if(!check_sim_ended(philo->data))
+        print_status(philo, "is thinking");
+}
+void philo_eat(t_philo *philo)
+{
+    if(!check_sim_ended(philo->data))
+    {
+        pthread_mutex_lock(&philo->meal_lock);
+        philo->last_meal_time = get_time_ms();
+        (philo->meals_eaten)++;
+        pthread_mutex_unlock(&philo->meal_lock);
+        print_status(philo, "is eating");
+        safe_usleep(philo->data->time_to_eat, philo->data);
+        pthread_mutex_unlock(&(philo->data->forks[philo->l_fork]));
+        pthread_mutex_unlock(&(philo->data->forks[philo->r_fork]));
+    }
+}
+
+void philo_take_forks(t_philo *philo)
+{
+    if(philo->data->number_of_philos == 1)
+    {
+	    pthread_mutex_lock(&(philo->data->forks[0]));
+	    print_status(philo, "has taken a fork");
+        return;        
+    }
+	pthread_mutex_lock(&(philo->data->forks[philo->l_fork]));
+	print_status(philo, "has taken a fork");
+    if(check_sim_ended(philo->data))
+    {
+	    pthread_mutex_unlock(&(philo->data->forks[philo->l_fork]));
+        return;   
+    }
+    pthread_mutex_lock(&(philo->data->forks[philo->r_fork]));
+    print_status(philo, "has taken a fork");
+    if(check_sim_ended(philo->data))
+    {
+        pthread_mutex_unlock(&(philo->data->forks[philo->l_fork]));
+        pthread_mutex_unlock(&(philo->data->forks[philo->r_fork]));
+        return;   
+    }        
+}
+
+void philo_sleep(t_philo *philo)
+{   
+    if(!check_sim_ended(philo->data))
+    {
+        print_status(philo, "is sleeping");
+        safe_usleep(philo->data->time_to_sleep, philo->data);
+    }
+}
